b061eb0d1e5fb9c002dc9c29d3083098608a81ec
Wendell Pereira <wendell@Wendells-Mac-mini>
2018-03-04 13:59:32 -0600

    master
    - Initial commmit;
    
    - Cria arquivo de instalação principal;
    
    - Cria arquivo de Todo.

---
+++ caskconfig.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+brew cask install smcfancontrol
+brew cask install sublime-text
+brew cask install vlc
+brew cask install adobe-creative-cloud
+brew cask install google-chrome
+brew cask install harvest
+brew cask install robo-3t
+brew cask install spotify
+brew cask install tweeten
+brew cask install vlc
+brew cask install webstorm
+brew cask install whatsapp
+brew cask install github
+
+# tornar executável: chmod a+x caskconfig.sh
+
+echo " "
+echo "<<ATENÇÃO>> Não tem no brew cask:"
+echo "----------------------"
+echo "Trello.app"
+echo "TweetDeck.app --> tweeten"
+echo ""
