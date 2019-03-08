116e868cbbcec0e2ec1ca57986487e702fb67860
Wendell Pereira <wendell.souza@gmail.com>
2019-03-07 19:13:58 -0600

    add Atom and sort the list.

--- instAll.sh
+++ instAll.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
-brew cask install smcfancontrol
-brew cask install sublime-text
-brew cask install vlc
+brew cask install a-slower-speed-of-light
 brew cask install adobe-creative-cloud
+brew cask install appcleaner
+brew cask install atom
+brew cask install evernote
+brew cask install firefox
+brew cask install firefox-developer-edition
+brew cask install github
+brew cask install google-backup-and-sync
 brew cask install google-chrome
 brew cask install harvest
+brew cask install iglance
+brew cask install kindle
+brew cask install postaman
 brew cask install robo-3t
+brew cask install skype
+brew cask install slack
+brew cask install smcfancontrol
 brew cask install spotify
+brew cask install smcfancontrol
+brew cask install sublime-text
 brew cask install tweeten
 brew cask install vlc
 brew cask install webstorm
 brew cask install whatsapp
-brew cask install github
-brew cask install a-slower-speed-of-light
-brew cask install evernote
-brew cask install skype
-brew cask install appcleaner
-brew cask install github
-brew cask install postaman
-brew cask install slack
-brew cask install google-backup-and-sync
+
+
 
 
 # tornar executável: chmod a+x caskconfig.sh
